b150eafe96febf042541ee618afc46cc8019e332
Vijay Yadav <vijayyadav1002@gmail.com>
2025-01-19 08:33:57 -0600

    Updated Resume

---
+++ _next/static/IGxczwkPeDGQOvSt0p-7z/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/948-cb6dcfd8483151cd.js","static/chunks/pages/index-97c454d88fe6f147.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
